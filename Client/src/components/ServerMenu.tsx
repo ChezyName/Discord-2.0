f34e0880c2c2f20ca0472c673453bc62f8a3ed1a
ChezyName <mineboy2468@gmail.com>
2025-01-02 23:44:08 -0600

    Added UI for Server Addition

--- Client/src/components/ServerMenu.tsx
+++ Client/src/components/ServerMenu.tsx
@@ -1,15 +1,42 @@
 import { useEffect, useState } from 'react'
-import { Button } from "@mui/material"
+import { Button, TextField, Modal, Box, Typography } from "@mui/material"
 
 import AddIcon from '@mui/icons-material/Add';
 
 const ServerMenu = ({setSearch}:any) => {
+  const [isModalOpen,setModalOpen] = useState(false);
+
   return (
-    <div style={{backgroundColor: "purple", width: "100%", height: "5%", marginBottom: "2%", minHeight: "40px",
-    display: 'flex', flexDirection: "row"}}>
-        {/** Open Modal Window for adding Server via URL / IP */}
-        <Button sx={{aspectRatio: "1"}}><AddIcon/></Button>
-    </div>
+    <>
+      <div style={{backgroundColor: "purple", width: "100%", height: "5%", marginBottom: "2%", minHeight: "40px",
+        display: 'flex', flexDirection: "row"}}>
+
+          {/** Open Modal Window for adding Server via URL / IP */}
+          <Button sx={{aspectRatio: "1"}} onClick={() => {setModalOpen(true);}}><AddIcon/></Button>
+          <TextField sx={{marginRight: "2%", marginLeft: "2%"}}
+            id="server-search" label="Search" type="search" variant='standard'
+            onChange={(event) => {
+              if(setSearch) setSearch(event.target.value);
+            }}/>
+      </div>
+
+      {/** Modal Window to add Server */}
+      <Modal open={isModalOpen} onClose={() => {setModalOpen(false);}}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={{display: 'flex', flexDirection: "column",
+          backgroundColor: 'yellow', width: "auto", height: "auto",
+          position: "absolute", left: "50%", top: "50%", transform: 'translate(-50%, -50%)',
+          border: "4px solid blue", padding: "5px"
+        }}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add New Server
+          </Typography>
+          <TextField label='Server Address' type='text' variant='standard'/>
+          <Button>Add Server</Button>
+        </Box>
+      </Modal>
+    </>
   )
 }
 
